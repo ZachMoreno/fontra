30e5626436d33555e06f3e5a78a632498a73906b
Zachariah Moreno <xachmoreno@gmail.com>
2015-06-18 16:44:14 -0700

    first commit, basic functionality & assets in place

---
+++ fontra.app.js
@@ -0,0 +1,10 @@
+angular.module('fontra', ['ngResource',
+						  'ui.router',
+						  'ngAnimate',
+						  'ngMaterial',
+						  'fontra.gallery'
+						 ])
+
+.config([function() {
+
+}]);
---
+++ views/gallery/gallery.js
@@ -0,0 +1,43 @@
+'use strict';
+
+angular.module('fontra.gallery', [])
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+		$stateProvider.state('gallery', {
+			url: '/gallery',
+			templateUrl: '/views/gallery/gallery.html',
+			controller: 'galleryController'
+		});
+
+		$urlRouterProvider.otherwise('/gallery');
+}])
+
+
+.factory('GoogleWebFontsAPI', ['$resource', function($resource) {
+  var remoteURL = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyAJPTfXDpEtRhd3Fcvk4peRX_6YEJlJLSY',
+
+    googleWebFontsAPI   = $resource(remoteURL,
+	                            {},
+	                            {
+	                              getAllTypefaces: {
+	                                method: 'GET',
+	                                isArray: false,
+	                                cache: true
+	                              }
+	                            }
+	                           );
+
+  return googleWebFontsAPI;
+}])
+
+
+.controller('galleryController', ['$scope', 'GoogleWebFontsAPI', function($scope, GoogleWebFontsAPI) {
+	GoogleWebFontsAPI.getAllTypefaces().$promise.then(function(promisedTypefaces) {
+		$scope.typefaces = promisedTypefaces.items;
+
+		console.log($scope.typefaces);
+	});
+
+	$scope.showSearch = false;
+}]);
